416ca150e836884df44d0be0be0ecfea4fffab7c
Ryan Contento <contento.rk@gmail.com>
2021-10-25 20:46:39 -0400

    setting up api

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5e855c807a605e0f241043e0abd31ca1&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
