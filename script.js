f4ab921ab912c7b47067861f7ca60bc422184e74
Ryan Contento <contento.rk@gmail.com>
2021-10-26 19:15:24 -0400

    get initial forms

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5e855c807a605e0f241043e0abd31ca1&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=5e855c807a605e0f241043e0abd31ca1&query="';
+
+// get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
